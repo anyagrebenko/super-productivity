593114340197ebc34674be4df916a957a164d026
anyagrebenko <a.grebenko@bk.ru>
2026-07-31 15:07:59 +0000

    Removed Update button and auto update

--- src/app/core/startup/startup.service.ts
+++ src/app/core/startup/startup.service.ts
@@ -35,7 +35,6 @@ import { DataInitStateService } from '../data-init/data-init-state.service';
 import { OnboardingHintService } from '../../features/onboarding/onboarding-hint.service';
 import { LocalRestApiHandlerService } from '../electron/local-rest-api-handler.service';
 import { CustomThemeService } from '../theme/custom-theme.service';
-import { UpdateCheckService } from '../update-check/update-check.service';
 
 const w = window as Window & { productivityTips?: string[][]; randomIndex?: number };
 
@@ -67,7 +66,6 @@ export class StartupService {
   private _chromeExtensionInterfaceService = inject(ChromeExtensionInterfaceService);
   private _projectService = inject(ProjectService);
   private _trackingReminderService = inject(TrackingReminderService);
-  private _updateCheckService = inject(UpdateCheckService);
   private _opLogStore = inject(OperationLogStoreService);
   private _legacyPfDb = inject(LegacyPfDbService);
   private _store = inject(Store);
@@ -129,7 +127,6 @@ export class StartupService {
     // deferred init
     window.setTimeout(async () => {
       this._trackingReminderService.init();
-      this._updateCheckService.init();
       this._checkAvailableStorage();
       this._initOfflineBanner();
 
